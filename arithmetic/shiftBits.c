0576aa877a32b87dc7b0106202193534dfbb43ea
Hamberfim <adhamlingithub@gmail.com>
2023-11-29 17:19:59 -0600

    shift bits

--- arithmetic/shiftBits.c
+++ arithmetic/shiftBits.c
@@ -9,7 +9,28 @@ int main()
 
     for (int i = 0; i < 32; i++)
     {
-        printf("Left shifted by %d: %08x | %u\n", i, num << i, num << i);
+        printf("Left shifted by %02d: %08x | %u\n", i, num << i, num << i);
+    }
+
+    unsigned int num2 = 100000000;
+
+    for (int i = 0; i < 32; i++)
+    {
+        printf("Right shifted by %02d: %08x | %d\n", i, num2 >> i, num2 >> i);
+    }
+
+    int num3 = 5;
+
+    for (int i = 0; i < 32; i++)
+    {
+        printf("Left shifted by %02d: %08x | %u\n", i, num3 << i, num3 << i);
+    }
+
+    int num4 = 100000000;
+
+    for (int i = 0; i < 32; i++)
+    {
+        printf("Right shifted by %02d: %08x | %d\n", i, num4 >> i, num4 >> i);
     }
 
     return 0;
