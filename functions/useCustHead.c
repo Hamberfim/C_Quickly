faa8501da7f2ef0e21ee7b0cc5322da2e9893828
Hamberfim <adhamlingithub@gmail.com>
2023-12-07 09:20:56 -0600

    recursive header function

---
+++ functions/useCustHead.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "utils.h"
+
+// function prototypes
+void getNum();
+
+int main()
+{
+
+    getNum();
+
+    return 0;
+}
+
+void getNum()
+{
+    int num;
+    char again;
+
+    // user input
+    printf("Enter an integer to be squared: \n");
+    scanf("%d", &num);
+    printf("%d squared is %d\n", num, square(num));
+
+    printf("Square another number? Y or N: \n");
+    scanf("%1s", &again);
+
+    if (again == 'Y' || again == 'y')
+    {
+        getNum();
+    }
+    else
+    {
+        return;
+    }
+}
