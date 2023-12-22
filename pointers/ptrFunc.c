56db1b9539bafbe6461fee78d0f463fa9cbeccdc
Hamberfim <adhamlingithub@gmail.com>
2023-12-22 10:28:48 -0600

    pointing to functions

---
+++ pointers/ptrFunc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// function prototype
+int bounce(int num);
+int caller(int (*function)(int), int b);
+
+int main()
+{
+    int num;
+    int (*fptr)(int) = bounce;
+
+    num = (*fptr)(10);
+    printf("Return value: %d\n", num);
+
+    num = caller(fptr, 5);
+    printf("Return value: %d\n", num);
+
+    return 0;
+}
+
+int bounce(int num)
+{
+    printf("\nReceived value: %d\n", num);
+    return ((3 * num) + 3);
+}
+
+int caller(int (*function)(int), int b)
+{
+    return (*function)(b);
+}
