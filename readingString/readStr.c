9cb0a9ca50cd532465496af7e40e4a4e5f0f5d48
Hamberfim <adhamlingithub@gmail.com>
2023-12-23 13:43:44 -0600

    reading string

--- readingString/readStr.c
+++ readingString/readStr.c
@@ -10,8 +10,18 @@ int main(void)
                         '\0'};
 
     char *betaPtr = "Beta";
-    printf("%s\n", alphaArr);
-    printf("%s\n", betaPtr);
+    printf("%s char array is %li bytes\n", alphaArr, sizeof(alphaArr));
+    printf("%s pointer is %li bytes\n", betaPtr, sizeof(betaPtr));
+
+    char str[51];
+    printf("Enter up to 50 characters with spaces\n");
+    // fgets(character-array, number-of-characters-to-read, where-to-read-from)
+    fgets(str, sizeof(str), stdin);
+
+    printf("fgets() reads: ");
+    puts(str);
+
+    printf("\nfgets() reads: %s and is %li bytes\n", str, sizeof(str));
 
     return 0;
 }
