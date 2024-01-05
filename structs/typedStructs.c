fb58df8f948f0f41b209fdc8eca682c4c217502b
Hamberfim <adhamlingithub@gmail.com>
2024-01-05 14:54:11 -0600

    typed structs

--- structs/typedStructs.c
+++ structs/typedStructs.c
@@ -22,7 +22,7 @@ int main() {
   bottom.y = 8;
   printf("Bottom x: %d, Bottom y: %d\n", bottom.x, bottom.y);
 
-  Box rect = {6, 12, 30, 20};
+  Box rect = {{6, 12}, {30, 20}};
   printf("Point ax: %d\n", rect.a.x);
   printf("Point ay: %d\n", rect.a.y);
   printf("Point bx: %d\n", rect.b.x);
