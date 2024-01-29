3f0c2f2b1bad9736a4c5233709320360ff94f1dd
Hamberfim <adhamlingithub@gmail.com>
2024-01-29 15:30:00 -0600

    practise

---
+++ __Practise__/tipCalc.c
@@ -0,0 +1,20 @@
+/* Name: tipCalc
+ * Author: Anthony Hamlin
+ * Description:
+ * This program calculate a tip and the total with tip based on user input.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+  // prompt for bill amount
+
+  // prompt for tip rate as a percent
+
+  // calculate tip - function call
+
+  // display tip and total amount with tip.
+
+  return 0;
+}
